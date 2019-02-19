5bf6d616e6a6a0f5daef87591235af839f67d183
Todd Carlson <tcarlson@carlsonsw.com>
2019-02-18 23:39:47 -0500

    started address ORM

--- models/entry.rb
+++ models/entry.rb
@@ -1,12 +1,6 @@
-class Entry
-  # These must be accessors since we mutate them
-  attr_accessor :name, :phone_number, :email
-
-  def initialize(name, phone_number, email)
-    @name = name
-    @phone_number = phone_number
-    @email = email
-  end
+ require 'bloc_record/base'
+ 
+ class Entry < BlocRecord::Base
 
   def to_s
     "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
