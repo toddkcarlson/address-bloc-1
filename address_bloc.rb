9eb7f6110d8cb0af1149035ba7188729a429f72a
Todd Carlson <tcarlson@carlsonsw.com>
2019-02-26 22:17:32 -0500

    select methods

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,6 +1,14 @@
 require_relative 'controllers/menu_controller'
 
+require 'bloc_record'
+
+STDOUT.sync = true
+
+BlocRecord.connect_to("db/address_bloc.sqlite")
+
 menu = MenuController.new
+
 system "clear"
 puts "Welcome to AddressBloc!"
-menu.main_menu
+
+menu.main_menu()
--- models/address_book.rb
+++ models/address_book.rb
@@ -22,33 +22,8 @@ require "csv"
   end
 
   def import_from_csv(file_name)
-    # Implementation goes here
-    csv_text = File.read(file_name)
-    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-    csv.each do |row|
-      row_hash = row.to_hash
-      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-    end
+     Entry.create(name: name, phone_number: phone, email: email, address_book_id: self.id)
   end
 
-  # Search AddressBook for a specific entry by name
-  def binary_search(name)
-    lower = 0
-    upper = entries.length - 1
 
-    while lower <= upper
-      mid = (lower + upper) / 2
-      mid_name = entries[mid].name
-
-      if name == mid_name
-        return entries[mid]
-      elsif name < mid_name
-        upper = mid - 1
-      elsif name > mid_name
-        lower = mid + 1
-      end
-    end
-
-    return nil
-  end
 end
